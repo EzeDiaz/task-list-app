225edc4e8e2c62bccda36089957defd7fcfb9987
EzeDiaz <ezequiel.dz@gmail.com>
2023-12-08 16:51:55 -0300

    Add db url log and specify database name

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -4,8 +4,9 @@ const url = process.env.ORMONGO_URL || 'mongodb://localhost:27017/myorganizer';
 let db = null;
 
 export async function connectDB() {
+  console.log('Database URL to use: ', url)
   if (db) return db;
-  let client = await MongoClient.connect(url, { useNewUrlParser: true });
+  let client = await MongoClient.connect(url, { useNewUrlParser: true, dbName: 'myorganizer' });
   db = client.db();
   console.log('Got DB,', db)
   return db
