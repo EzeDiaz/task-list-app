ded7964838bbaad406fba97909aed3378889c5cc
EzeDiaz <ezequiel.dz@gmail.com>
2023-12-08 17:18:40 -0300

    Add Bootstrap styles

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -3,25 +3,27 @@ import * as mutations from '../store/mutations';
 import { connect } from 'react-redux';
 
 const LoginComponent = ({ authenticateUser, authenticated }) => {
-  return <div>
+  return <div className="card p-3 col-6">
     <h2>
       Please login
     </h2>
     <form onSubmit={ authenticateUser }>
       <input 
+        className="form-control"
         type="text"
         placeholder="username"
         name="username"
         defaultValue="Dev"
       />
       <input
+        className="form-control mt-2"
         type="password"
         placeholder="password"
         name="password"
         defaultValue=""
       />
       {authenticated === mutations.NOT_AUTHENTICATED ? <p>Login Incorrect</p> : null}
-      <button type="submit">Login</button>
+      <button type="submit" className="form-control mt-2 btn btn-primary">Login</button>
     </form>
   </div>
 };
